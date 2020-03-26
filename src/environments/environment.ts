83c4928ee8fd4d1109abb843587ea72db7faa04d
yurakohut <yurakohut97@gmail.com>
2020-03-26 16:51:12 +0200

    added all app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAr4rCmBWA5hOsELwEH_xfrrmsbXE6kQVQ",
+    authDomain: "taskmanager-5a302.firebaseapp.com",
+    databaseURL: "https://taskmanager-5a302.firebaseio.com",
+    projectId: "taskmanager-5a302",
+    storageBucket: "taskmanager-5a302.appspot.com",
+    messagingSenderId: "793821623941",
+    appId: "1:793821623941:web:f013549f9ae6570ecf5bec",
+    measurementId: "G-5DJQ89LC3N"
+
+  }
 };
 
 /*
